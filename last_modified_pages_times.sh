74b82a161fe8aa9292dad0cc4ad5793b2f7c42c1
Claudio Pastorini <pastorini.claudio@gmail.com>
2016-11-06 16:53:09 +0100

    Fix date format for sitemaps.xml

--- last_modified_pages_times.sh
+++ last_modified_pages_times.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-git ls-tree -r --name-only HEAD pages | while read filename; do echo "$(git log -1 --format="%ad" --date=iso -- "$filename") $filename"
+git ls-tree -r --name-only HEAD pages | while read filename; do echo "$(git log -1 --format="%ad" --date=short -- "$filename") $filename"
 done
