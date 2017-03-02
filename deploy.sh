ccf199beb9c5e7ec946f27f04ad885fa494d5335
Claudio Pastorini <pastorini.claudio@gmail.com>
2017-03-02 17:22:37 +0100

    Update site

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/home/claudio/Enviroments/porfolio/bin/python /home/claudio/PycharmProjects/claudiopastorini.github.io/site.py build
+/home/claudio/Enviroments/portfolio/bin/python3.6 /home/claudio/PycharmProjects/claudiopastorini.github.io/site.py build
 git add * 
 git commit -m "Update site"
 git subtree split --prefix dist -b master
